0b4939658e78e10471110fb43bb32686fa1eaa82
Matheus Alcantara <73028088+mattalcantara@users.noreply.github.com>
2022-05-31 19:02:46 -0300

    certin

--- AC2/enviar_email.py
+++ AC2/enviar_email.py
@@ -24,3 +24,27 @@ def enviar_email(email_cliente, produto):
         print ("Email sent successfully!")
     except Exception as ex:
         print ("Something went wrong….",ex)
+
+def enviar_email_cancelamento(email_cliente, produto):
+    gmail_user = 'mslogosteste@gmail.com'
+    gmail_password = 'Teste@MsLogos'
+    sent_from = gmail_user
+    to = [email_cliente]
+    subject = 'E-mail de cancelamento de Chamado \n \n'
+    body = f'Nos sentimos muito, o seu chamado sobre o produto {produto} foi cancelado.'
+    email_text = """\
+    From: %s
+    %s
+    %s
+
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+    try:
+        smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        smtp_server.ehlo()
+        smtp_server.login(gmail_user, gmail_password)
+        smtp_server.sendmail(sent_from, to, email_text)
+        smtp_server.close()
+        print ("Email sent successfully!")
+    except Exception as ex:
+        print ("Something went wrong….",ex)
